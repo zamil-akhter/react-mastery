8168cb5809117eed7c0169e0dfb1026325bedabe
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-29 23:33:37 +0530

    add arrange the card horizontally

--- 03-props-and-tailwind/src/App.jsx
+++ 03-props-and-tailwind/src/App.jsx
@@ -1,14 +1,16 @@
-import Card from "./components/card"
+import Card from "./components/card";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#07110f] px-4 py-8 flex flex-col items-center justify-center">
-      <h2 className="text-emerald-400 font-bold text-2xl my-4">
-        React with tailwind and props
-      </h2>
-      <Card />
-    </div>
-  )
+    <>
+      <h2 className="text-emerald-400 font-bold text-2xl my-4">React with tailwind and props</h2>
+      <div className="min-h-screen bg-[#07110f] px-4 py-8 flex items-center justify-between gap-4 flex-wrap">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;

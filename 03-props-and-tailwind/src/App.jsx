712e9a1b4d22981a4eb1329f93b632f6754d7318
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-29 23:41:17 +0530

    add dynamic and defaukt props

--- 03-props-and-tailwind/src/App.jsx
+++ 03-props-and-tailwind/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
     <>
       <h2 className="text-emerald-400 font-bold text-2xl my-4">React with tailwind and props</h2>
       <div className="min-h-screen bg-[#07110f] px-4 py-8 flex items-center justify-between gap-4 flex-wrap">
-        <Card />
-        <Card />
-        <Card />
+        <Card fullName="Zamil Akhter" btnText="Visit me"/>
+        <Card fullName="Tanveer Azam"/>
+        <Card fullName="Irfan Perwez"/>
       </div>
     </>
   );
--- 03-props-and-tailwind/src/components/card.tsx
+++ 03-props-and-tailwind/src/components/card.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-export default function Card() {
+export default function Card({fullName = 'John Doe', btnText="Read more"}) {
   return (
     <div className="w-full max-w-sm overflow-hidden rounded-xl border border-emerald-800/60 bg-gradient-to-br from-[#11221d] via-[#163129] to-[#0d1a16] text-white shadow-2xl shadow-emerald-950/40">
       <img src="https://picsum.photos/301" alt="" className="h-44 w-full object-cover object-center" />
       <div className="flex flex-col justify-between space-y-8 p-6">
         <div className="space-y-2">
-          <h2 className="text-3xl font-semibold tracking-wide text-emerald-300">Lorem</h2>
+          <h2 className="text-3xl font-semibold tracking-wide text-emerald-300">{fullName}</h2>
           <p className="text-sm leading-6 text-emerald-100/80">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum soluta amet corporis accusantium aliquid consectetur eaque!</p>
         </div>
         <button type="button" className="flex w-full items-center justify-center rounded-md bg-emerald-600 p-3 font-semibold tracking-wide text-emerald-50 transition hover:bg-emerald-500">
-          Read more
+          {btnText}
         </button>
       </div>
     </div>

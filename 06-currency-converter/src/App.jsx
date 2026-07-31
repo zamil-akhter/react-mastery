4ea8830732e8f6883a47c53d75ba36cb338349a6
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-31 20:58:18 +0530

    add project setup for currency converter

---
+++ 06-currency-converter/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-900 h-screen w-screen flex items-center justify-center">
+      <div className="bg-gray-500 text-white w-fit h-fit flex justify-center items-center p-10 rounded-lg">
+        <h2>Currency converter</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;

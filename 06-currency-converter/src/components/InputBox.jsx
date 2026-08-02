ab8bc4169dc007e5d172aa8a8c0700ef7513f36f
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-02 13:20:13 +0530

    add connection of tags using useId hook

--- 06-currency-converter/src/components/InputBox.jsx
+++ 06-currency-converter/src/components/InputBox.jsx
@@ -1,11 +1,15 @@
+import { useId } from "react";
 import React from "react";
 
 function InputBox({ label, amount, onAmountChange, onCurrencyChange, currencyOptions = [], selectedCurrency = "usd", amountDisabled = false, currencyDisabled = false, className = "" }) {
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex `}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
-        <input className="outline-none w-full bg-transparent py-1.5" type="number" placeholder="Amount" disabled={amountDisabled} value={amount} onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} />
+        <label className="text-black/40 mb-2 inline-block" htmlFor={amountInputId}>
+          {label}
+        </label>
+        <input className="outline-none w-full bg-transparent py-1.5" type="number" placeholder="Amount" disabled={amountDisabled} value={amount} onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} id={amountInputId} />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>

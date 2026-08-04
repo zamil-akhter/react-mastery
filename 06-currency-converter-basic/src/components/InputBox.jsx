e328c94f3c52cb11f4817da3bd81076039e2bffc
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-04 22:22:04 +0530

    add prettier

--- 06-currency-converter-basic/src/components/InputBox.jsx
+++ 06-currency-converter-basic/src/components/InputBox.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 
-function InputBox({
-  label,
-  currencyOptions = [],
-  amount,
-  onAmountChange,
-  isInputDisable,
-  selectedCurrency = "usd",
-  onCurrencyChange,
-}) {
+function InputBox({ label, currencyOptions = [], amount, onAmountChange, isInputDisable, selectedCurrency = "usd", onCurrencyChange }) {
   return (
     <div className="bg-gray-700 p-4 w-fit h-fit rounded-sm">
       <div className="flex justify-between">
@@ -16,19 +8,8 @@ function InputBox({
         <p className="text-gray-300">Currency type</p>
       </div>
       <div className="flex justify-between">
-        <input
-          type="text"
-          placeholder="Amount"
-          value={amount}
-          disabled={isInputDisable}
-          onChange={(e) => onAmountChange?.(Number(e.target.value))}
-        />
-        <select
-          name=""
-          id=""
-          value={selectedCurrency}
-          onChange={(e) => onCurrencyChange?.(e.target.value)}
-        >
+        <input type="text" placeholder="Amount" value={amount} disabled={isInputDisable} onChange={(e) => onAmountChange?.(Number(e.target.value))} />
+        <select name="" id="" value={selectedCurrency} onChange={(e) => onCurrencyChange?.(e.target.value)}>
           {currencyOptions.map((opt) => (
             <option value={opt} key={opt}>
               {opt}

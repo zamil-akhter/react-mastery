d8b0a8cde4fc2acfae747126aeafb7cf1d0b519b
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-07 00:40:49 +0530

    add basic react router project setup

---
+++ 08-react-router-basic/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <h2>Welcome to the App</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;

51035cb16207f60e5a6d8c348bd66587928626ba
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-08 01:01:39 +0530

    add router using BrowserRouter

--- 08-browser-router/src/App.jsx
+++ 08-browser-router/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      <div className="">
-        <h2>Welcome to the App</h2>
-      </div>
+      {/* Navigation bar - always visible */}
+      <nav className="bg-red-200 py-3 flex justify-center gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
---
+++ 08-browser-router/src/components/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-600 text-3xl text-green-500 font-bold">
+      <h2>About screen</h2>
+    </div>
+  );
+}
+
+export default About;

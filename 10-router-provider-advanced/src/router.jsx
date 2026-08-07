2b092fdd6560544a9fc5a89dc69b998d1872c144
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-08 01:30:46 +0530

    add structure for components and children

---
+++ 10-router-provider-advanced/src/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
+import { About, Contact, Home } from "./components";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default router;

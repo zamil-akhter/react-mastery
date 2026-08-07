5e561c894132a19013b02111f20105323eb7c64f
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-08 01:29:05 +0530

    make main file to enable for routing using RouterProvider

--- 10-router-provider-advanced/src/main.jsx
+++ 10-router-provider-advanced/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);

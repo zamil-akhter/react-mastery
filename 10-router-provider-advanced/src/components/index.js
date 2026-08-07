92b30d988387bac07671376e38f28e8b91b1509e
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-08 01:27:49 +0530

    add components

---
+++ 10-router-provider-advanced/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./header/Header";
+import Home from "./Home/Home";
+import Footer from "./footer/Footer";
+import About from "./Home/About";
+import Contact from "./Home/Contact";
+
+export { Header, Home, Footer, About, Contact };

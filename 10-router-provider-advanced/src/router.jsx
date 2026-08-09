9e354579a8445f2f328d3e7003b15676c6403a33
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-09 13:02:01 +0530

    update router to create using createRoutesFromElements function

--- 10-router-provider-advanced/src/router.jsx
+++ 10-router-provider-advanced/src/router.jsx
@@ -1,26 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Layout from "./Layout";
 import { About, Contact, Home } from "./components";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+// Other way to create a router with the same configuration using the `createRoutesFromElements` function
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>,
+  ),
+);
 
 export default router;

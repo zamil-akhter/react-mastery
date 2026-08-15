33b97a39efcbf2048608d1796c05330faac4f43d
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-16 00:56:24 +0530

    add theme change functionality

--- 13-theme-switcher-tailwind/src/App.jsx
+++ 13-theme-switcher-tailwind/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./context/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // Actual change the theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode]);
+
   return (
-    <>
-      <h2 className="">Theme Swithcher</h2>
-    </>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">{/* Theme button */}</div>
+          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 

0670d2e20fc4b8a29d8e2bf4da9be7dcd1c45de5
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-16 01:34:01 +0530

    add components

--- 13-theme-switcher-tailwind/src/App.jsx
+++ 13-theme-switcher-tailwind/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeButton";
+import Card from "./components/Card";
 
 function App() {
   const [themeMode, setThemeMode] = useState("light");
@@ -16,15 +18,19 @@ function App() {
   // Actual change the theme
   useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector('html').classList.add(themeMode)
+    document.querySelector("html").classList.add(themeMode);
   }, [themeMode]);
 
   return (
     <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="flex flex-wrap min-h-screen items-center bg-white dark:bg-gray-900">
         <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">{/* Theme button */}</div>
-          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>

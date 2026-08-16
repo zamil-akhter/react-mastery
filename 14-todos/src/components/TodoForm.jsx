49cacaec8d51f370f0bf35a1d5e7b941a96a478b
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-16 13:31:28 +0530

    add todo form feature

---
+++ 14-todos/src/components/TodoForm.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useTodo } from "../context";
+
+function TodoForm() {
+  const [todoContent, setTodoContent] = React.useState("");
+
+  const { addTodo } = useTodo();
+
+  const add = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({ todoContent, completed: false });
+    setTodoContent("");
+  };
+
+  return (
+    <form className="flex" onSubmit={add}>
+      <input type="text" placeholder="Write Todo..." value={todoContent} onChange={(e) => setTodoContent(e.target.value)} className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5" />
+      <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;

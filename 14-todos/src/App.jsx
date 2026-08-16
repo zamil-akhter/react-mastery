8e36826bffba1a5f1aa93b13f3b62bdfdb82a2b5
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-16 11:55:06 +0530

    add todo context and update app ui

--- 14-todos/src/App.jsx
+++ 14-todos/src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import { TodoProvider } from "./context";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <>
-      <h1>Todo App</h1>
-    </>
+    <TodoProvider value={{ todos, addTodo, editTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">{/*Loop and Add TodoItem here */}</div>
+        </div>
+      </div>
+    </TodoProvider>
   );
 }
 

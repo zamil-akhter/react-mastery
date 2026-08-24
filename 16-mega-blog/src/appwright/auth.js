5ae465f528b3957e0a97a4266da5fbb1a7dc8ba3
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-25 00:32:35 +0530

    add authentication service through appwrite

---
+++ 16-mega-blog/src/appwright/auth.js
@@ -0,0 +1,62 @@
+import config from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client.setEndpoint(config.appwriteUrl).setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const createdUser = await this.account.create({
+        id: ID.unique(),
+        email,
+        password,
+        name,
+      });
+      if (createdUser) {
+        // Calling login after successful account creation
+        return this.login({ email, password });
+      } else {
+        return createdUser;
+      }
+    } catch (error) {
+      console.error("Error creating account:", error);
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession({ email, password });
+    } catch (error) {
+      console.error("Error logging in:", error);
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.error("Error getting current user:", error);
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.error("Error logging out:", error);
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+export default authService;

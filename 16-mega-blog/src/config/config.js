5f58822b02615df15be2af6ecd05bf2fd2edbf9b
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-25 00:31:48 +0530

    add stringify for the env config

--- 16-mega-blog/src/config/config.js
+++ 16-mega-blog/src/config/config.js
@@ -1,15 +1,14 @@
 const config = {
-    appwriteUrl: import.meta.env.VITE_APPWRITE_URL,
-    appwriteProjectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    appwriteDatabaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    appwriteCollectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
-    appwriteBucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID
-
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
-    console.log('Appwrite URL:', config.appwriteUrl);
-    console.log('Appwrite Project ID:', config.appwriteProjectId);
-    console.log('Appwrite Database ID:', config.appwriteDatabaseId);
-    console.log('Appwrite Collection ID:', config.appwriteCollectionId);
-    console.log('Appwrite Bucket ID:', config.appwriteBucketId);   
+console.log("Appwrite URL:", config.appwriteUrl);
+console.log("Appwrite Project ID:", config.appwriteProjectId);
+console.log("Appwrite Database ID:", config.appwriteDatabaseId);
+console.log("Appwrite Collection ID:", config.appwriteCollectionId);
+console.log("Appwrite Bucket ID:", config.appwriteBucketId);
 
 export default config;

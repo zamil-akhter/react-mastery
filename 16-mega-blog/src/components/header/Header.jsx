e106b3649fb368dcd9dc102199ab29c702ba968f
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-29 23:16:41 +0530

    add headers

---
+++ 16-mega-blog/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "../index";
+
+function Header() {
+  return (
+    <div>
+      <Container>
+        <div>Header</div>
+      </Container>
+    </div>
+  );
+}
+
+export default Header;

649d3a42952e3eb4ec87d5d6f977ac871df60846
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-08-31 23:20:53 +0530

    add fixes in app & add store in main

--- 16-mega-blog/src/App.jsx
+++ 16-mega-blog/src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwright/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { Footer, Header } from "./components";
 
 function App() {
-  return (
-    <>
-      <h2>A blog app with appwrite</h2>
-      <h2>A blog app with appwrite</h2>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          console.log("Logged in user:", userData);
+          dispatch(login({ userData }));
+        } else {
+          console.log("No user is currently logged in.");
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>Todos{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;

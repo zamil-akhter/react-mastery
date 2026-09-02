4f9ba672307e7d9ba93c4bd4520b5d47be78ec02
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-09-02 23:45:34 +0530

    changes env naming for table

--- 16-mega-blog/src/config/config.js
+++ 16-mega-blog/src/config/config.js
@@ -2,13 +2,13 @@ const config = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
 console.log("Appwrite URL:", config.appwriteUrl);
 console.log("Appwrite Project ID:", config.appwriteProjectId);
 console.log("Appwrite Database ID:", config.appwriteDatabaseId);
-console.log("Appwrite Collection ID:", config.appwriteCollectionId);
+console.log("Appwrite Table ID:", config.appwriteTableId);
 console.log("Appwrite Bucket ID:", config.appwriteBucketId);
 
 export default config;

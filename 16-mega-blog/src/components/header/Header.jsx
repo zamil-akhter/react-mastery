40855c4d54d5518500d4fbaecf51b91836c9d1bf
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-09-03 23:18:29 +0530

    add header section

--- 16-mega-blog/src/components/header/Header.jsx
+++ 16-mega-blog/src/components/header/Header.jsx
@@ -1,13 +1,48 @@
-import React from "react";
-import { Container } from "../index";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+
+  const navigate = useNavigate();
+  const navItems = [
+    { name: "Home", slug: "/", active: true },
+    { name: "Login", slug: "/login", active: !authStatus },
+    { name: "Signup", slug: "/signup", active: !authStatus },
+    { name: "All Posts", slug: "/all-posts", active: authStatus },
+    { name: "Add Post", slug: "/add-post", active: authStatus },
+  ];
+
   return (
-    <div>
+    <header className="py-3 bg-gray-500">
       <Container>
-        <div>Header</div>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.slug}>
+                  <button onClick={() => navigate(item.slug)} className="inline-block px-6 py-2 duration-200 rounded-full hover:bg-blue-100">
+                    {item.name}
+                  </button>
+                  {authStatus && (
+                    <li>
+                      <LogoutBtn />
+                    </li>
+                  )}
+                </li>
+              ) : null,
+            )}
+          </ul>
+        </nav>
       </Container>
-    </div>
+    </header>
   );
 }
 

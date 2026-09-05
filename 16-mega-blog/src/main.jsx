b36b8ced026228dc69e44b636fc9f1698d6919a6
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-09-05 23:20:35 +0530

    add post card & update main for routing

--- 16-mega-blog/src/main.jsx
+++ 16-mega-blog/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 );

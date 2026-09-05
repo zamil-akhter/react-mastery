44bccac45804275fa982caddc0cb934c8277cc9d
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-09-05 23:19:48 +0530

    add reusable input box

---
+++ 16-mega-blog/src/components/Input.jsx
@@ -0,0 +1,17 @@
+import { forwardRef, useId } from "react";
+
+const Input = forwardRef(function Input({ label, type = "text", className = "", ...props }, ref) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1 pl-1" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input type={type} className={`w-full px-3 py-2 border rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border-gray-200 ${className}`} ref={ref} id={id} {...props} />
+    </div>
+  );
+});
+
+export default Input;

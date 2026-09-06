28f5006444450bcf1cdaa141e365477b0302edbb
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-09-06 23:50:21 +0530

    add login component

--- 16-mega-blog/src/components/index.js
+++ 16-mega-blog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button };
